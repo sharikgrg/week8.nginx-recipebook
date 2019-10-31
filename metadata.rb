4133ad1ff91f44093b96b1ec285d858bb7f39dbc
Sharik Gurung <SGurung@spartaglobal.com>
2019-10-31 11:15:02 +0000

    changed the dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description 'Installs/Configures node5'
 long_description 'Installs/Configures node5'
 version '0.1.0'
 chef_version '>= 14.0'
-depends 'apt'
-depends 'nodejs', '~> 6.0.0'
+# depends 'apt'
+# depends 'nodejs', '~> 6.0.0'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
